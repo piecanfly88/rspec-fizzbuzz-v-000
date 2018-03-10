6ba267449e321e160634f446cb307a2fac0cd8e0
Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-10 23:46:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
-    else if int % 3 == 0
+    else
+      if int % 3 == 0
       puts "Fizz"
-      else if int % 5 == 0
+      else
+        if int % 5 == 0
         puts "Buzz"
       end
     end
   end
 end
-
-
