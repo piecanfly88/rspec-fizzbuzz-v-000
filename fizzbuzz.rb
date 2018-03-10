75e7317284d0e6eb1b6fe3169671619070afebe4
Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-10 23:52:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  else
-    if int % 3 == 0
-      "Fizz"
-    else
-      if int % 5 == 0
-        "Buzz"
-      end
-    end
-  end
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
 end
